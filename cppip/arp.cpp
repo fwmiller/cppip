fada921e6608f3a5fcb265ce08211474492c042a
Frank W. Miller <frank@frankwmiller.net>
2021-01-06 20:41:58 -0700

    Added gen_arp_packet() routine

--- cppip/arp.cpp
+++ cppip/arp.cpp
@@ -34,22 +34,32 @@ arp::receive()
 	}
 }
 
-void
-arp::send_probe()
+static void
+gen_arp_packet(uint8_t *buf, uint16_t opcode,
+	uint8_t *sha, uint32_t spa, uint8_t *tha, uint32_t tpa)
 {
-	// Generate ARP probe packet
-
-	uint8_t buf[ETH_MTU_SIZE];
-	memset(buf, 0, ETH_MTU_SIZE);  // Zeros ah->tha
 	arp_hdr_t ah = (arp_hdr_t)(buf + sizeof(struct eth_hdr));
 	ah->hw = reverse_byte_order_short(ARP_HW_ETH);
 	ah->proto = reverse_byte_order_short(ETHERTYPE_ARP);
 	ah->hw_addr_len = ETH_ADDR_LEN;
 	ah->proto_addr_len = IPV4_ADDR_LEN;
-	ah->opcode = reverse_byte_order_short(ARP_OP_REQ);
-	memcpy(ah->sha, my_addr->get_ha(), ETH_ADDR_LEN);
-	ah->spa = 0;
-	ah->tpa = ah->spa;
+	ah->opcode = reverse_byte_order_short(opcode);
+	if (sha != NULL)
+		memcpy(ah->sha, sha, ETH_ADDR_LEN);
+	ah->spa = spa;
+	if (tha != NULL)
+		memcpy(ah->tha, tha, ETH_ADDR_LEN);
+	ah->tpa = tpa;
+}
+
+void
+arp::send_probe()
+{
+	// Generate ARP probe packet
+	uint8_t buf[ETH_MTU_SIZE];
+	memset(buf, 0, ETH_MTU_SIZE);  // Zeros ah->tha
+	gen_arp_packet(buf, ARP_OP_REQ,
+		my_addr->get_ha(), 0, NULL, 0);
 }
 
 void
@@ -57,16 +67,9 @@ arp::send_announce()
 {
 	// Generate ARP REQ announcement packet
 	//  Note: ARP REP announcement method not supported
-
 	uint8_t buf[ETH_MTU_SIZE];
 	memset(buf, 0, ETH_MTU_SIZE);  // Zeros ah->tha
-	arp_hdr_t ah = (arp_hdr_t)(buf + sizeof(struct eth_hdr));
-	ah->hw = reverse_byte_order_short(ARP_HW_ETH);
-	ah->proto = reverse_byte_order_short(ETHERTYPE_ARP);
-	ah->hw_addr_len = ETH_ADDR_LEN;
-	ah->proto_addr_len = IPV4_ADDR_LEN;
-	ah->opcode = reverse_byte_order_short(ARP_OP_REQ);
-	memcpy(ah->sha, my_addr->get_ha(), ETH_ADDR_LEN);
-	ah->spa = reverse_byte_order_short(my_addr->get_pa());
-	ah->tpa = ah->spa;
+	gen_arp_packet(buf, ARP_OP_REQ,
+		my_addr->get_ha(), reverse_byte_order_short(my_addr->get_pa()),
+		NULL, reverse_byte_order_short(my_addr->get_pa()));
 }
