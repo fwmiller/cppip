0c37bdf41fa746f9509f7fd63f56b6660605a610
Frank W. Miller <frank@frankwmiller.net>
2022-03-05 18:20:22 -0800

    Added udptab

---
+++ include/udptab.h
@@ -0,0 +1,19 @@
+#ifndef __UDPTAB_H
+#define __UDPTAB_H
+
+#include "inq.h"
+
+static const int UDPTAB_ENTRIES = 16;
+
+class udptab {
+private:
+	class inq table[UDPTAB_ENTRIES];
+
+public:
+	udptab();
+	void dump();
+	class inq *find_port(uint16_t port);
+	class inq *alloc_port(uint16_t port);
+};
+
+#endif
---
+++ src/udp/udptab.cpp
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include "udptab.h"
+
+udptab::udptab()
+{
+	return;
+}
+
+void
+udptab::dump()
+{
+	return;
+}
+
+class inq *
+udptab::find_port(uint16_t port)
+{
+	return NULL;
+}
+
+class inq *
+udptab::alloc_port(uint16_t port)
+{
+	return NULL;
+}
