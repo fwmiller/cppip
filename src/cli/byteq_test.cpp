41b615246dcf76173e8d3c7d8b7123808694a605
Frank W Miller <frank@frankwmiller.net>
2022-03-30 03:31:34 +0000

    Added timeout to byteq test

--- src/cli/byteq_test.cpp
+++ src/cli/byteq_test.cpp
@@ -1,13 +1,16 @@
 #include "byteq.h"
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 void
 byteq_test() {
     byteq q;
     char ch = 'a';
     char recv;
+    time_t start, curr;
 
-    for (;;) {
+    for (start = time(NULL);;) {
         for (int i = 0; i < 10; i++) {
             q.append((buf_t) &ch, 1);
             q.dump_contents();
@@ -22,6 +25,12 @@ byteq_test() {
             q.remove((buf_t) &recv, 1);
             q.dump_contents();
             printf("\r");
+        }
+
+        curr = time(NULL);
+        if (curr - start == 5) {
+            printf("\r\n");
+            break;
         }
     }
 }
